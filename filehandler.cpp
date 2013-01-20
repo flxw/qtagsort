819819b29c319b395d26d4bb151feec2c19c30be
flxw <ich@flxw.de>
2013-01-20 15:11:46 +0000

    Reduce doubled functionality from FileHandler class
    MusicDataModel now does several things that FileHandler did
    until now. These, like expanding the pattern, have been removed.

--- filehandler.cpp
+++ filehandler.cpp
@@ -3,90 +3,38 @@
 # include <QDir>
 # include <QFile>
 
-
-/* ========================================== */
-/* init placeholders */
-const QString FileHandler::PH_ARTIST  = QString("%a");
-const QString FileHandler::PH_ALBUM   = QString("%r");
-const QString FileHandler::PH_TITLE   = QString("%t");
-const QString FileHandler::PH_TRACKNO = QString("%n");
-const QString FileHandler::PH_YEAR    = QString("%y");
-
-
-FileHandler::FileHandler(QObject *parent) : QObject(parent) {
-    this->renameFiles = false;
+FileHandler::FileHandler(MusicDataModel *datm, QObject *parent) : QObject(parent) {
+    this->dm = datm;
 }
 
 /* =========================================== */
 /*      implementation of public functions     */
 /* =========================================== */
-bool FileHandler::isReady(void) {
-    return (!this->targetDir.isEmpty() && !this->sourceFileList.isEmpty() && !this->pattern.isEmpty());
-}
-
-void FileHandler::setPattern(const QString &p) {
-    this->pattern = p;
-
-    /* check for file extension at the end */
-    this->renameFiles = pattern.endsWith(".ext");
-}
-
-void FileHandler::setSources(const QStringList &sl) {
-    this->sourceFileList = sl;
-}
-
-const QStringList& FileHandler::getSources(void) const{
-    return this->sourceFileList;
-}
-
-void FileHandler::addToSources(const QString &src) {
-    this->sourceFileList.append(src);
-}
-
-void FileHandler::setTargetDir(const QString &t) {
-    this->targetDir = t;
-}
 /* =========================================== */
 /*      implementation of public slots         */
 /* =========================================== */
 void FileHandler::startSortAction(void) {
-    QStringList::Iterator strIt = this->sourceFileList.begin();
-    QString expandedPattern, curLoc;
+    QList<MusicDataModel::MusicFileData>::const_iterator dbIt = this->dm->getDBstart();
 
     QFile file;
-    QFileInfo fileInfo;
     QDir directory;
 
     this->filesCopied  = 0;
     this->copiesFailed = 0;
-    const float perc = 100/this->sourceFileList.length();
+    const float perc = 100/this->dm->rowCount(QModelIndex());
 
-    for(int i = 1; strIt != this->sourceFileList.end(); ++strIt, ++i) {
-        file.setFileName(*strIt);
-        fileInfo.setFile(file);
-        TagLib::FileRef fr(strIt->toStdString().c_str(), false);
+    /* everything is initalized, now let the pattern expansion begin */
+    this->dm->prepareData();
 
-        /* check for nonexistent tags and empty files */
-        if (fr.isNull()) {
-            ++this->copiesFailed;
-            continue;
-        } else if (fr.tag()->isEmpty()) {
-            ++this->copiesFailed;
-            continue;
-        }
-
-        /* expand pattern get absolute filepath or directory - depending on mode */
-        expandedPattern = (this->renameFiles) ? this->expandPattern(fr, fileInfo) : \
-                                                this->expandPattern(fr,fileInfo).append(fileInfo.fileName());
-        curLoc          = fileInfo.absoluteFilePath();
-
-        if (expandedPattern != this->pattern && expandedPattern != curLoc) {
+    for (int i = 1; dbIt != this->dm->getDBend(); ++dbIt, ++i) {
+        if (dbIt->isGood) {
             /* find the last directory delimiter */
-            const unsigned int pos = expandedPattern.lastIndexOf("/");
+            file.setFileName(dbIt->location);
+            const unsigned int pos = dbIt->destination.lastIndexOf("/");
 
             /* create the directory and move the file */
-            if (directory.mkpath(expandedPattern.left(pos))) {
-                if (file.copy(expandedPattern)) {
+            if (directory.mkpath(dbIt->destination.left(pos))) {
+                if (file.copy(dbIt->destination)) {
                     ++this->filesCopied;
                 } else {
                     ++this->copiesFailed;
@@ -106,31 +54,3 @@ void FileHandler::startSortAction(void) {
 /* =========================================== */
 /*      implementation of private functions    */
 /* =========================================== */
-QString FileHandler::expandPattern(const TagLib::FileRef &fr, const QFileInfo &fI) {
-    QString exp = this->pattern;
-    exp.replace("/", "\n"); // workaround for slashes in tags - replace the path slashes with NULL
-
-    if (!fr.tag()->artist().isEmpty())
-        exp.replace(FileHandler::PH_ARTIST, fr.tag()->artist().toCString(true));
-
-    if (!fr.tag()->album().isEmpty())
-        exp.replace(FileHandler::PH_ALBUM, fr.tag()->album().toCString(true));
-
-    if (!fr.tag()->title().isEmpty())
-        exp.replace(FileHandler::PH_TITLE, fr.tag()->title().toCString(true));
-
-    if (fr.tag()->track())
-        exp.replace(FileHandler::PH_TRACKNO, QString("%1").arg(fr.tag()->track()));
-
-    if (fr.tag()->year())
-        exp.replace(FileHandler::PH_YEAR, QString("%1").arg(fr.tag()->year()));
-
-    if (this->renameFiles)
-        exp.replace(".ext", fI.suffix().prepend("."));
-
-    /* check if part of the pattern could not be expanded */
-    if (exp.contains("%"))
-        return this->pattern;
-    else /* replace tag slashes with underscore and rereplace NULLs with slashes */
-        return this->targetDir + "/" + exp.replace("/", "_").replace("\n", "/");
-}
--- filehandler.h
+++ filehandler.h
@@ -2,11 +2,11 @@
 #define FILEHANDLER_H
 
 # include <QObject>
-# include <QStringList>
-# include <QFileInfo>
 
 # include <taglib/fileref.h>
 # include <taglib/tag.h>
+
+# include "musicdatamodel.h"
 
 class FileHandler : public QObject
 {
@@ -14,7 +14,6 @@ class FileHandler : public QObject
 
 public:
     enum HandleResult {FGOOD, FEMPTY, FTEMPTY, FBAD};
-    const static QString PH_ARTIST, PH_ALBUM, PH_TITLE, PH_TRACKNO, PH_YEAR;
 
     struct HandleReport {
         unsigned int handled;
@@ -24,17 +23,7 @@ public:
 /* =========================================== */
 /*           methods are defined below         */
 public:
-    explicit FileHandler(QObject *parent = 0);
-
-    bool isReady(void);
-
-    /* getters and setters */
-    void setPattern(const QString &p);
-    void setSources(const QStringList &sl);
-    void addToSources(const QString &src);
-    const QStringList& getSources(void) const;
-
-    void setTargetDir(const QString &t);
+    explicit FileHandler(MusicDataModel *datm, QObject *parent = 0);
 
 signals:
     void fileHandled(FileHandler::HandleResult);
@@ -44,16 +33,11 @@ signals:
 public slots:
     void startSortAction(void);
 
-private:
-    QString expandPattern(const TagLib::FileRef &fr, const QFileInfo &fI);
-
 /* =========================================== */
 /*        attributes are defined below         */
 private:
     /* variables for core functionality */
-    QStringList  sourceFileList;
-    QString       targetDir;
-    QString       pattern;
+    MusicDataModel *dm;
 
     unsigned int filesCopied, copiesFailed;
     bool          renameFiles;
