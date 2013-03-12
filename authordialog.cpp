cb55eea47f923ee80ea1797d792a1e6f87d04420
flxw <felix@flxw.de>
2013-03-12 11:40:21 +0100

    Fix display of correct version information in authordialog

--- authordialog.cpp
+++ authordialog.cpp
@@ -1,11 +1,13 @@
-#include "authordialog.h"
-#include "ui_authordialog.h"
+# include "authordialog.h"
+# include "ui_authordialog.h"
+# include "versioninfo.h"
 
 AuthorDialog::AuthorDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AuthorDialog)
 {
     ui->setupUi(this);
+    this->ui->versionLabel->setText(QString(VERSION));
 }
 
 AuthorDialog::~AuthorDialog()
