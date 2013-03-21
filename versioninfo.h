d4327f64163538e0c4dbcc7402baddb0df63020b
flxw <ich@flxw.de>
2013-03-21 16:14:13 +0100

    VERSION BUMP TO 0.1!

--- versioninfo.h
+++ versioninfo.h
@@ -1,7 +1,9 @@
+/* project start: 02.01.2013
+ * last release:  20.03.2013 */
 #ifndef VESIONINFO_H
 #define VESIONINFO_H
 
-# define PROGNAME "tagman"
-# define VERSION  "the beta"
+# define PROGNAME "qtagsort"
+# define VERSION  "0.1"
 
 #endif // VESIONINFO_H
