a27a1741f2079406bb20a2f2e3f463d3eac557c2
flxw <ich@flxw.de>
2013-04-10 23:43:14 +0200

    UPDATE TO VERSION 0.2 =)

--- versioninfo.h
+++ versioninfo.h
@@ -1,9 +1,10 @@
 /* project start: 02.01.2013
- * last release:  20.03.2013 */
+ * last release:  10.04.2013 */
+
 #ifndef VESIONINFO_H
 #define VESIONINFO_H
 
 # define PROGNAME "qtagsort"
-# define VERSION  "0.1"
+# define VERSION  "0.2"
 
 #endif // VESIONINFO_H
