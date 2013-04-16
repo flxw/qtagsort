922fbfa71d8cd38f772541846c5dfdbaf8f82e4f
flxw <ich@flxw.de>
2013-04-16 22:18:03 +0200

    Prepare v0.3 release!

--- versioninfo.h
+++ versioninfo.h
@@ -1,10 +1,10 @@
 /* project start: 02.01.2013
- * last release:  10.04.2013 */
+ * last release:  16.04.2013 */
 
 #ifndef VESIONINFO_H
 #define VESIONINFO_H
 
 # define PROGNAME "qtagsort"
-# define VERSION  "0.2"
+# define VERSION  "0.3"
 
 #endif // VESIONINFO_H
