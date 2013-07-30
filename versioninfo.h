610b96a6bd6783168b6bca332e39260df904c9ce
flxw <felix@flxw.de>
2013-07-30 19:33:43 +0200

    Amend version info

--- versioninfo.h
+++ versioninfo.h
@@ -1,10 +1,10 @@
 /* project start: 02.01.2013
- * last release:  16.04.2013 */
+ * last release:  08.08.2013 */
 
 #ifndef VESIONINFO_H
 #define VESIONINFO_H
 
 # define PROGNAME "qtagsort"
-# define VERSION  "0.3"
+# define VERSION  "0.4"
 
 #endif // VESIONINFO_H
